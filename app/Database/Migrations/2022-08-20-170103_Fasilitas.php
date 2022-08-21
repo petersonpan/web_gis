9ace667f0cc1b336cc9c1db554b1928351b634a3
Nahasrobert <obetjembatanmerah@gmail.com>
2022-08-21 12:26:54 +0800

    merge

--- app/Database/Migrations/2022-08-20-170103_Fasilitas.php
+++ app/Database/Migrations/2022-08-20-170103_Fasilitas.php
@@ -10,30 +10,30 @@ class Fasilitas extends Migration
     {
         $this->forge->AddField([
 
-            'id_Fasilitas'          => [
-            'type'              => 'INT',
-            'constraint'        => 5,
-            'unsigned'          =>true,
-            'auto_increment'    =>true
+            'id_fasilitas'          => [
+                'type'              => 'INT',
+                'constraint'        => 5,
+                'unsigned'          => true,
+                'auto_increment'    => true
             ],
 
             'keterangan'        => [
                 'type'          => 'text',
                 'null'          => true,
-                ],    
+            ],
             'created_at' => [
                 'type' => 'DATETIME',
                 'NULL' => true,
             ],
-            
+
             'updated_at' => [
                 'type' => 'DATETIME',
                 'NULL' => true,
             ],
         ]);
 
-        $this->forge->addKey('id_Fasilitas',TRUE);
-        $this->forge->createTable('fasilitas',TRUE);
+        $this->forge->addKey('id_fasilitas', TRUE);
+        $this->forge->createTable('fasilitas', TRUE);
     }
 
     public function down()
@@ -41,4 +41,3 @@ class Fasilitas extends Migration
         $this->forge->dropTable('fasilitas');
     }
 }
-
--- app/Database/Migrations/2022-08-20-170135_TempatWisata.php
+++ app/Database/Migrations/2022-08-20-170135_TempatWisata.php
@@ -11,36 +11,36 @@ class TempatWisata extends Migration
         $this->forge->AddField([
 
             'id_tempat'          => [
-            'type'              => 'INT',
-            'constraint'        => 5,
-            'unsigned'          =>true,
-            'auto_increment'    =>true
+                'type'              => 'INT',
+                'constraint'        => 5,
+                'unsigned'          => true,
+                'auto_increment'    => true
             ],
 
             'nama_tempat'        => [
                 'type'          => 'VARCHAR',
                 'constraint'    => 50,
-                ],
+            ],
             'id_kecamatan'        => [
                 'type'          => 'BIGINT',
                 'constraint'    => '5',
                 'unsigned'      => true,
-                ],    
+            ],
             'keterangan'        => [
                 'type'          => 'text',
                 'null'          => true,
-                ],   
-                
-            'jarak' =>[
-                'type' =>'text',
-                'null' =>true
+            ],
+
+            'jarak' => [
+                'type' => 'text',
+                'null' => true
             ],
 
             'created_at' => [
                 'type' => 'DATETIME',
                 'NULL' => true,
             ],
-            
+
             'updated_at' => [
                 'type' => 'DATETIME',
                 'NULL' => true,
@@ -48,9 +48,9 @@ class TempatWisata extends Migration
 
         ]);
 
-        $this->forge->addKey('id_tempat',TRUE);
-        $this->forge->addForeignKey('id_kecamatan','kecamatan','id_kecamatan');
-        $this->forge->createTable('tempat_wisata',TRUE);
+        $this->forge->addKey('id_tempat', TRUE);
+        $this->forge->addForeignKey('id_kecamatan', 'kecamatan', 'id_kecamatan');
+        $this->forge->createTable('tempat_wisata', TRUE);
     }
 
     public function down()
@@ -58,5 +58,3 @@ class TempatWisata extends Migration
         $this->forge->dropTable('tempat_wisata');
     }
 }
-
-
